2546cec07bb67409aecd2a81d43c58235298366c
yongxiaodong <1317665590@qq.com>
2020-11-06 23:27:35 +0800

    新增prodjava

---
+++ jumpserver/sce_key.sh
@@ -0,0 +1,14 @@
+if [ ! "$SECRET_KEY" ]; then
+  SECRET_KEY=`cat /dev/urandom | tr -dc A-Za-z0-9 | head -c 50`;
+  echo "SECRET_KEY=$SECRET_KEY" >> ~/.bashrc;
+  echo $SECRET_KEY;
+else
+  echo $SECRET_KEY;
+fi
+if [ ! "$BOOTSTRAP_TOKEN" ]; then
+  BOOTSTRAP_TOKEN=`cat /dev/urandom | tr -dc A-Za-z0-9 | head -c 16`;
+  echo "BOOTSTRAP_TOKEN=$BOOTSTRAP_TOKEN" >> ~/.bashrc;
+  echo $BOOTSTRAP_TOKEN;
+else
+  echo $BOOTSTRAP_TOKEN;
+fi
